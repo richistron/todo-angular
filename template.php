73498b4e441e54df644727132bb4f92aa519509a
Ricardo Rivas González <richistron@gmail.com>
2012-12-30 18:22:57 -0600

    base style added to template

---
+++ template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>        
+        <meta charset="UTF-8">
+        <meta name="description" content="El blog del richistron">
+        <meta name="keywords" content="richistron">
+        <meta name="author" content="Ricardo Rivas">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link href="css/style.css" rel="stylesheet/less" media="screen">
+        <title>richistron.com | Otro dude más en el interwebs</title>
+    </head>
+    <body>  
+        <header class="logo">
+            <h1>Richistron.com</h1>
+            <p>Otro dude más en el interwebs</p>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#sitios">Sitios</a></li>
+                <li><a href="#noticias">Noticias</a></li>
+                <li><a href="#blogs">Blogs</a></li>
+                <li><a href="#comunidades">Comunidades</a></li>
+            </ul>                                
+        </nav>
+        <div id="container">
+            <section id="sitios">Sitios</section>
+            <section id="noticias">Noticias</section>
+            <section id="blogs">Blogs</section>
+            <section id="comunidades">Comunidades</section>
+        </div>                    
+        <footer>Footer</footer>
+        <script src="js/libs-no-bootstrap.js"></script>
+        <!--<script src="js/fancyRss.js"></script>-->
+        <!--<script src="js/front.js"></script>-->
+    </body>
+</html>
