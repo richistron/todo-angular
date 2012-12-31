e3b32682acadef94403f17a9f02916510db705c5
Ricardo Rivas González <richistron@gmail.com>
2012-12-31 07:55:35 -0600

    header styled

--- template.php
+++ template.php
@@ -11,24 +11,35 @@
         <title>richistron.com | Otro dude más en el interwebs</title>
     </head>
     <body>  
-        <header>
+        <header class="mainheader">
             <h1>Richistron.com</h1>
             <p>Otro dude más en el interwebs</p>
             <a href="#" class="logo">http://richistron.com/</a>
         </header>
         <nav>
             <ul>
-                <li><a href="#sitios">Sitios</a></li>
-                <li><a href="#noticias">Noticias</a></li>
-                <li><a href="#blogs">Blogs</a></li>
-                <li><a href="#comunidades">Comunidades</a></li>
+                <li><a href="#">Comunidades</a></li>                
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Blogs</a></li>                
             </ul>                                
         </nav>
         <div id="container">
-            <section id="sitios">Sitios</section>
-            <section id="noticias">Noticias</section>
-            <section id="blogs">Blogs</section>
-            <section id="comunidades">Comunidades</section>
+            <section id="sitios">
+                <div class="box">
+                    <article>
+                        <header>                            
+                            <h1>10 PHP code quality tools to avoid a mess in your projects</h1>
+                        </header>                        
+                        <img src="/img/cats/120x120.jpg" alt="logo"/>
+                        <p>
+                            When programming in any language there are certain common errors that everyone makes as they mature and evolve their ...
+                        </p>
+                        <aside>
+                            <a href="#" class="readmore">Leer más</a> <time datetime="2011-01-26" pubdate>Monday, January 26,2011</time> <span class="author"> Author: <strong> Fulanito </strong>                        
+                        </aside>
+                    </article>
+                </div>
+            </section>            
         </div>                    
         <footer>Footer</footer>
         <script src="js/libs-no-bootstrap.js"></script>
