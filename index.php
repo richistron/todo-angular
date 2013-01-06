4f16d5c410075893f9a3bf5cb7d28d619a6937d9
Ricardo Rivas González <richistron@gmail.com>
2013-01-06 12:15:03 -0600

    new colors

--- index.php
+++ index.php
@@ -1,166 +1,52 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
+    <head>        
+        <meta charset="UTF-8">
+        <meta name="description" content="El blog del richistron">
+        <meta name="keywords" content="richistron">
+        <meta name="author" content="Ricardo Rivas">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link href="css/style.css" rel="stylesheet/less" media="screen">
         <title>richistron.com | Otro dude más en el interwebs</title>
-        <meta charset="utf-8" />
-        <meta name="description" content="El blog del richistron"/>
-        <meta name="keywords" content="richistron"/>
-        <meta name="author" content="Ricardo Rivas"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="shortcut icon" href="img/favicon.ico"/>      
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-        <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen"/>
-        <link href="css/customized.less" rel="stylesheet/less" type="text/css" media="screen"/>
     </head>
-    <body>        
-        <div class="navbar navbar-inverse navbar-fixed-top visible-phone">
-            <div class="navbar-inner">                
-                <div class="pull-right">
-                    <a class="btn btn-navbar"  href="#modalMenu" role="button" data-toggle="modal">
-                        <span class="icon-align-justify icon-white">
-                            &nbsp;
-                        </span>                        
-                    </a>
-                </div>                                        
-            </div>
-        </div>        
-        <div class="row-fluid visible-phone">&nbsp;</div>
-        <div class="container-fluid" id="main">            
-            <div id="header" class="row-fluid">                
-                <div class="thumbnail">
-                    <img data-src="holder.js/960x230" src="img/cats/960x230.jpg" alt="banner">
-                </div>
-                <div class="hero-unit">                    
-                    <h1>Richistron.com</h1>
-                    <p>Otro dude más en el interwebs</p>                    
-                </div>
-            </div>            
-            <div class="row-fluid" id="content">
-                <div class="span3 pull-right hidden-phone" id="rightCol">
-                    <div data-spy="affix" data-offset-top="0">
-                        <div class="thumbnail">
-                            <img src="img/cats/200x200.jpg" data-src="holder.js/200x200" alt="ad 200x200" />
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion span9 pull-left" id="secciones">
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordionBtnA" data-toggle="collapse" data-parent="#secciones" href="#blogs">
-                                <dl class="dl-horizontal">
-                                    <dt>Blogs</dt>
-                                    <dd class="muted">Tuerce los blogs recomendados ;)</dd>
-                                </dl>                                
-                            </a>
-                        </div>
-                        <div id="blogs" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <div class="row-fluid rssItem" id="gabo" data-feedUrl="http://feeds.feedburner.com/nethazard?format=xml" data-feedLogo="img/cats/120x120.jpg"></div>
-                                <div class="row-fluid rssItem" id="richistron" data-feedUrl="http://blog.richistron.com/feeds/posts/default" data-feedLogo="img/logo120.png"></div>                                            
-                                <div class="row-fluid rssItem" id="levhita" data-feedUrl="http://blog.levhita.net/feed/" data-feedLogo="img/cats/120x120.jpg"></div>
-                                <div class="row-fluid rssItem" id="bbhx" data-feedUrl="http://briceno.mx/feed/" data-feedLogo="img/cats/120x120.jpg"></div>                                
-                            </div>
-                        </div>
-                        <div class="accordion-group">
-                            <div class="accordion-heading">
-                                <a class="accordionBtnA" data-toggle="collapse" data-parent="#secciones" href="#comunidades">
-                                    <dl class="dl-horizontal">
-                                        <dt>Comunidades</dt>
-                                        <dd class="muted">Encuentra grupos y enterate de las las platicas</dd>
-                                    </dl>                                
-                                </a>                            
-                            </div>
-                            <div id="comunidades" class="accordion-body collapse">
-                                <div class="accordion-inner">
-                                    Chocolate caramels sugar plum ice cream gummi bears gummi bears sesame snaps lemon drops. Cake wafer jelly-o tart chupa chups applicake marzipan apple pie. Dessert dragée ice cream sweet roll chupa chups wypas bear claw.
-                                    Gummies oat cake cheesecake toffee tootsie roll icing macaroon bear claw dessert. Jujubes wypas candy pudding caramels cheesecake cheesecake. Wafer cheesecake chupa chups caramels carrot cake jelly beans.
-                                    Wafer gummi bears toffee fruitcake jelly danish. Cheesecake topping chupa chups sesame snaps sweet toffee. Chocolate cake tootsie roll cotton candy cotton candy bonbon. Candy cupcake oat cake.
-                                    Gingerbread candy canes tootsie roll fruitcake carrot cake oat cake sugar plum. Fruitcake halvah jujubes apple pie gingerbread toffee cheesecake. Biscuit ice cream wypas cotton candy icing ice cream chocolate cake gummies.
-                                    Sweet candy croissant sugar plum dessert lemon drops topping. Chocolate jelly brownie tootsie roll powder sweet roll carrot cake faworki. Jelly beans brownie faworki.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-group">
-                            <div class="accordion-heading">                    
-                                <a class="accordionBtnA" data-toggle="collapse" data-parent="#secciones" href="#noticias">
-                                    <dl class="dl-horizontal">
-                                        <dt>Noticias</dt>
-                                        <dd class="muted">RSS de las noticias mas ñoñas y así.</dd>
-                                    </dl>                                
+    <body>  
+        <header class="mainheader">
+            <h1>Richistron.com</h1>
+            <p>Otro dude más en el interwebs</p>
+            <a href="#" class="logo">http://richistron.com/</a>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">Comunidades</a></li>                
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Blogs</a></li>                
+            </ul>                                
+        </nav>
+        <div id="container">
+            <section id="sitios">
+                <div class="box">
+                    <article>
+                        <header>                            
+                            <h1>
+                                <a href="#">
+                                    10 PHP code quality tools to avoid a mess in your projects
                                 </a>
-                            </div>
-                            <div id="noticias" class="accordion-body collapse">
-                                <div class="accordion-inner">
-                                    Chocolate caramels sugar plum ice cream gummi bears gummi bears sesame snaps lemon drops. Cake wafer jelly-o tart chupa chups applicake marzipan apple pie. Dessert dragée ice cream sweet roll chupa chups wypas bear claw.
-                                    Gummies oat cake cheesecake toffee tootsie roll icing macaroon bear claw dessert. Jujubes wypas candy pudding caramels cheesecake cheesecake. Wafer cheesecake chupa chups caramels carrot cake jelly beans.
-                                    Wafer gummi bears toffee fruitcake jelly danish. Cheesecake topping chupa chups sesame snaps sweet toffee. Chocolate cake tootsie roll cotton candy cotton candy bonbon. Candy cupcake oat cake.
-                                    Gingerbread candy canes tootsie roll fruitcake carrot cake oat cake sugar plum. Fruitcake halvah jujubes apple pie gingerbread toffee cheesecake. Biscuit ice cream wypas cotton candy icing ice cream chocolate cake gummies.
-                                    Sweet candy croissant sugar plum dessert lemon drops topping. Chocolate jelly brownie tootsie roll powder sweet roll carrot cake faworki. Jelly beans brownie faworki.
-                                </div>
-                            </div>
+                            </h1>
+                        </header>                        
+                        <div class="thumb">
+                            <img src="/img/cats/120x120.jpg" alt="logo"/>
                         </div>
-                    </div>
+                        <p>
+                            When programming in any language there are certain common errors that everyone makes as they mature and evolve their ...
+                            <a href="#" class="readmore">Leer más</a>
+                        </p>                        
+                        <span class="author"> Author: <strong> Fulanito </strong> | <time datetime="2011-01-26">Monday, January 26,2011</time></span>
+                    </article>
                 </div>
-                <div id="footer" class="row-fluid">
-                    <div class="span9">
-                        <p class="muted credit">
-                            Powered by 
-                            <a href="http://twitter.com/richistron">
-                                @richistron
-                            </a>
-                            . Free Open Source Software, get the code on 
-                            <a href="https://github.com/richistron/richistron-frontend">
-                                GitHub
-                            </a>
-                            .
-                        </p>
-                    </div>
-                </div>            
-            </div>        
-            <div class="navbar navbar-inverse navbar-fixed-bottom visible-phone">
-                <div class="navbar-inner">                
-                    <div class="pull-right">
-                        <a class="btn btn-navbar"  href="#modalMenu" role="button" data-toggle="modal">
-                            <span class="icon-align-justify icon-white">
-                                &nbsp;
-                            </span>                        
-                        </a>
-                    </div>                                        
-                </div>
-            </div>
-            <div id="modalMenu" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="menuTitle" aria-hidden="true">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 id="menuTitle"> <span class="icon-align-justify">&nbsp;</span> <span>Menu</span></h4>
-                </div>
-                <div class="modal-body">
-                    <ul class="nav nav-list">
-                        <li>
-                            <a class="modalBtn btn btn-large btn-block disabled" data-dismiss="modal" aria-hidden="true" data-parent="#secciones" href="#blogs">
-                                Blogs
-                            </a>
-                        </li>
-                        <li>
-                            <a class="modalBtn btn btn-large btn-block disabled" data-dismiss="modal" aria-hidden="true" data-parent="#secciones" href="#comunidades">
-                                Comunidades
-                            </a>
-                        </li>
-                        <li>
-                            <a class="modalBtn btn btn-large btn-block disabled" data-dismiss="modal" aria-hidden="true" data-parent="#secciones" href="#noticias">
-                                Noticias
-                            </a>
-                        </li>                        
-                    </ul>
-                    <div class="modal-footer">
-                      <a class="btn btn-large btn-block disabled" data-dismiss="modal" aria-hidden="true">
-                        Close <span class="icon-remove">&nbsp;</span>
-                      </a>
-                    </div>
-                </div>
-              </div>                            
-            </div>
-            <script src="js/libs.js"></script>                       
-            <script src="js/fancyRss.js"></script>
-            <script src="js/front.js"></script>
+            </section>            
+        </div>                    
+        <footer>Footer</footer>
+        <script src="js/libs.js"></script>
     </body>
 </html>
