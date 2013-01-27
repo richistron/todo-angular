ff62763676ef5c4e947ace89bf7b08d8e8666b56
Ricardo Rivas <richistron@gmail.com>
2013-01-27 08:11:38 -0600

    tmp commit

--- index.php
+++ index.php
@@ -7,47 +7,36 @@
         <meta name="author" content="Ricardo Rivas">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="img/favicon.ico">
-        <link href="css/style.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="css/style.css" rel="stylesheet" media="screen">        
         <title>richistron.com | Otro dude más en el interwebs</title>
     </head>
     <body>  
-        <header class="mainheader">
-            <h1>Richistron.com</h1>
-            <p>Otro dude más en el interwebs</p>
-            <a href="#" class="logo">http://richistron.com/</a>
-        </header>
-        <nav>
-            <ul>
-                <li><a href="#">Comunidades</a></li>                
-                <li><a href="#">Noticias</a></li>
-                <li><a href="#">Blogs</a></li>                
-            </ul>                                
-        </nav>
-        <div id="container">
-            <section id="sitios">
-                <div class="box">
-                    <article>
-                        <header>                            
-                            <h1>
-                                <a href="#">
-                                    10 PHP code quality tools to avoid a mess in your projects
-                                </a>
-                            </h1>
-                        </header>                        
-                        <div class="thumb">
-                            <img src="/img/cats/120x120.jpg" alt="logo"/>
-                        </div>
-                        <p>
-                            When programming in any language there are certain common errors that everyone makes as they mature and evolve their ...
-                            <a href="#" class="readmore">Leer más</a>
-                        </p>                        
-                        <span class="author"> Author: <strong> Fulanito </strong> | <time datetime="2011-01-26">Monday, January 26,2011</time></span>
-                    </article>
+        <div class="row-fluid">
+            <header>
+                <div class="container">
+                    <a href="#">
+                        <h1>Richistron.com</h1>
+                        <p>Otro dude más en el interwebs</p>
+                    </a>
                 </div>
-            </section>            
-        </div>                    
-        <footer>Footer</footer>
+            </header>
+            <nav>
+                <div class="container">
+                    <ul class="nav nav-pills">
+                      <li class="active">
+                        <a href="#">Home</a>
+                      </li>
+                      <li><a href="#">Blogs</a></li>
+                      <li><a href="#">Noticias</a></li>
+                    </ul>                               
+                </div>
+            </nav>
+            <section class="container">
+                contenido
+            </section>                    
+            <footer class="container">Footer</footer>
+        </div>        
         <script src="js/libs.js"></script>
-        <script src="js/core.js"></script>        
     </body>
 </html>
