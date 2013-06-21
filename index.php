dda6be6aa0b906b92f942a00424ec5be4df1e0bc
Ricardo Rivas <richistron@gmail.com>
2013-06-21 12:43:08 -0500

    analytics changed

--- index.php
+++ index.php
@@ -67,6 +67,15 @@
         <script src="js/views-min-v2.0.1.js"></script>
         <script src="js/collections-min-v2.0.1.js"></script>
         <script src="js/core-min-v2.0.1.js"></script>
-        <script src="js/analytics.js"></script>        
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-12736297-8']);
+          _gaq.push(['_trackPageview']);
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>       
     </body>
 </html>
