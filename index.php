a39bd39591d8499197627d964e702da21ec7185d
Ricardo Rivas G <richistron@gmail.com>
2013-06-30 11:00:07 -0500

    less js added to development porposes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta name="author" content="Ricardo Rivas">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="img/favicon.ico">
-        <link href="css/style.css" rel="stylesheet" media="screen">
+        <link href="css/style.less" type="text/css" rel="stylesheet/less" media="screen">
         <title>richistron.com | Otro dude más en el interwebs</title>
     </head>
     <body>  
@@ -62,7 +62,7 @@
                 </a>
             </div>
         </footer>        
-        <script src="js/libs-v2.0.1.js"></script>        
+        <script src="js/libs-v2.2.1.js"></script>        
         <script src="js/app/collections/main.js"></script>
         <script src="js/app/models/main.js"></script>
         <script src="js/app/views/main.js"></script>
